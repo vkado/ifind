2d8f393e52d1a3c25a854fb208bf10fe7c919621
parinya_pan <parinya_pany@truecorp.co.th>
2015-05-09 14:21:41 +0700

    GGGGG

--- web_ifind/application/views/map/map.php
+++ web_ifind/application/views/map/map.php
@@ -206,14 +206,15 @@ if(empty($order)){
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="myModalLabel">Contact Information</h4>
       </div>
       <div class="modal-body">
-        ...
+         ITRUEMART <br>
+         02-999-9999
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary">CALL</button>
       </div>
     </div>
   </div>
